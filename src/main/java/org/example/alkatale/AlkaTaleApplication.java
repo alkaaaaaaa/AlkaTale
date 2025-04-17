5cfee848fbfd217b789f14615175ca0ddc2b9ed2
alka <hkagf123456@qq.com>
2025-04-17 09:16:30 +0800

    提交

---
+++ src/main/java/org/example/alkatale/AlkaTaleApplication.java
@@ -0,0 +1,13 @@
+package org.example.alkatale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkaTaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkaTaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/alkatale/AlkaTaleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.alkatale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkaTaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
